9ef830ceee4774a5d1a1d9579018f0b90de187cf
Zakoulov <zakoylov@gmail.com>
2020-02-15 20:12:37 +0300

    Add vk api sdk

--- app/src/main/java/ru/zakoulov/vkcupa/App.kt
+++ app/src/main/java/ru/zakoulov/vkcupa/App.kt
@@ -6,5 +6,6 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
     }
 }
--- app/src/main/java/ru/zakoulov/vkcupa/MainActivity.kt
+++ app/src/main/java/ru/zakoulov/vkcupa/MainActivity.kt
@@ -1,18 +1,43 @@
 package ru.zakoulov.vkcupa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAccessToken
+import com.vk.api.sdk.auth.VKAuthCallback
+import com.vk.api.sdk.auth.VKScope
 import ru.zakoulov.vkcupa.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!VK.isLoggedIn()) {
+            VK.login(this, arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+        }
+
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val callback = object: VKAuthCallback {
+            override fun onLogin(token: VKAccessToken) {
+
+            }
+
+            override fun onLoginFailed(errorCode: Int) {
+
+            }
+        }
+        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 }
