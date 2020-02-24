63bb21cf4a3c2f9009e2606e79c2b05953837a0a
Zakoulov <zakoylov@gmail.com>
2020-02-25 02:25:55 +0300

    Add product list layout

---
+++ app/src/main/java/ru/zakoulov/vkcupg/utils/SquareRelativeLayout.kt
@@ -0,0 +1,17 @@
+package ru.zakoulov.vkcupg.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+open class SquareRelativeLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+): RelativeLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
+}
